3b404e116a75f8b97f63fe45b770b3e39821b593
leandro.witzke <leandro.witzke@lodgify.com>
2025-11-05 20:12:04 +0100

    feat: update the configuration

--- obj/Debug/net8.0/SemanticVersioning.AssemblyInfo.cs
+++ obj/Debug/net8.0/SemanticVersioning.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SemanticVersioning")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a51d19416ea1fe87dfbd3f1a44708e1a7f61a2f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("SemanticVersioning")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SemanticVersioning")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
