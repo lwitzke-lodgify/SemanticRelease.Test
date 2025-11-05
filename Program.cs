278cd0b7e28651e9a86bc2b95c65cb1bc2d45187
leandro.witzke <leandro.witzke@lodgify.com>
2025-11-05 20:48:48 +0100

    fix: is it working

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
-﻿Console.WriteLine("This is the semantic versioning project");
-Console.WriteLine("Is it works? ");
+﻿Console.WriteLine("Is it works? ");
 Console.WriteLine("Is it works? ");
 Console.WriteLine("Is it works? ");
 Console.WriteLine("Is it works? ");
 
+Console.WriteLine("This is the semantic versioning project");
